f505e88addb76c0843de0d240ebcf32fd1998427
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2024-03-08 17:58:55 +0530

    Update App.jsx

--- 2-bharat-clock-version-one/src/App.jsx
+++ 2-bharat-clock-version-one/src/App.jsx
@@ -1,13 +1,18 @@
-
+import ClockHeading from './components/ClockHeading'
+import ClockSlogan from './components/ClockSlogan'
+import CurrentTime from './components/CurrentTime'
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 
 function App() {
   
 
   return (
-    <div>
-      Clock
-    </div>
+    <center>
+      <ClockHeading></ClockHeading>
+      <ClockSlogan></ClockSlogan>
+      <CurrentTime></CurrentTime>
+    </center>
   )
 }
 
