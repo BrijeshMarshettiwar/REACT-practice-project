9c0c797c9daeeb80bcf52d56cc128b59d41f3523
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2024-03-12 22:27:04 +0530

    Update App.jsx

--- 3-Fragments/src/App.jsx
+++ 3-Fragments/src/App.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
+import FoodItems from './components/FoodItems';
+import ErrorMessage from './components/ErrorMessage';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 
 function App() {
-  
+  // let foodItem = [];
+  let foodItem = ["Dal","Green Vegetable","Roti","Salad","Milk","Ghee"];
+
+  // if (foodItem.length === 0){
+  //   return <h3>I am still hungry</h3>
+  // }
+
+  // let emptyMessage = foodItem.length === 0 ? <h3>I am still hungry</h3> : null;
 
   return (
     <>
-      <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Dal</li>
-        <li class="list-group-item">Green Vegetable</li>
-        <li class="list-group-item">Roti</li>
-        <li class="list-group-item">Salad</li>
-        <li class="list-group-item">Milk</li>
-      </ul>
+      <h1 className='foodheading'>Healthy Food</h1>
+      {/* {foodItem.length === 0 ? <h3>I am still hungry</h3> : null
+        } */}
+      {/* {emptyMessage} */}
+
+      {/* {foodItem.length === 0 && <h3>I am still hungry</h3> } */}
+        <ErrorMessage items={foodItem}></ErrorMessage>
+
+        <FoodItems items={foodItem}></FoodItems>
     </>
   )
 }
