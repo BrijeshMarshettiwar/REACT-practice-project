c9bd5c2522d7422918fe9a87cd2c233730d6ee41
Brijesh Marshettiwar <123184136+BrijeshMarshettiwar@users.noreply.github.com>
2024-03-13 21:59:45 +0530

    Update Item.jsx

--- 3-Fragments/src/components/Item.jsx
+++ 3-Fragments/src/components/Item.jsx
@@ -2,9 +2,18 @@ import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) =>{
 
+  const handleBuyButtonClick = (event) =>{
+    console.log(event);
+    console.log(`${foodItem} being bought`);
+  }
+
   return (
     <li className={`${styles["Kg-item"]} list-group-item`}>
-      <span className={styles["Kg-span"]}>{foodItem}</span></li>
+      <span className={styles["Kg-span"]}>{foodItem}</span>
+      <button className={`${styles.button} btn btn-info`}
+      onClick={(event) => handleBuyButtonClick(event)}
+      >Buy</button>
+    </li>
   )
 }
 export default Item;
